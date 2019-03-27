4252b5003f35019b09ac696914320a1452819207
Russell Hicks <russell.hicks@bondhealth.co.uk>
2019-03-27 17:39:52 +0000

    Mockito added

--- TipCalculator/app/src/test/java/com/acme/tipcalculator/viewmodel/CalculatorViewModelTest.kt
+++ TipCalculator/app/src/test/java/com/acme/tipcalculator/viewmodel/CalculatorViewModelTest.kt
@@ -1,15 +1,25 @@
 package com.acme.tipcalculator.viewmodel
 
+import com.acme.tipcalculator.model.RestaurantCalculator
+import com.acme.tipcalculator.model.TipCalculation
 import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
 
 class CalculatorViewModelTest {
 
     lateinit var calculatorViewModel: CalculatorViewModel
 
+    @Mock
+    lateinit var mockCalculator : RestaurantCalculator
+
     @Before
     fun setup(){
+        MockitoAnnotations.initMocks(this)
         calculatorViewModel = CalculatorViewModel()
     }
 
@@ -18,16 +28,30 @@ class CalculatorViewModelTest {
 
         calculatorViewModel.inputCheckAmount = "10.00"
         calculatorViewModel.inputTipPercentage = "15"
+
+        val stub = TipCalculation(10.00, 15,1.50, 11.5)
+        `when`(mockCalculator.calculateTip(10.00, 15)).thenReturn(stub)
+
         calculatorViewModel.calculateTip()
 
-        assertEquals(10.00, calculatorViewModel.tipCalculation.checkAmount)
-        assertEquals(15, calculatorViewModel.tipCalculation.tipPct)
-        assertEquals(1.50, calculatorViewModel.tipCalculation.tipAmount)
-        assertEquals(11.50, calculatorViewModel.tipCalculation.grandTotal)
-
+        assertEquals(stub, calculatorViewModel.tipCalculation)
 
 
     }
+
+    @Test
+    fun testCalculateBadTipPercent() {
+
+        calculatorViewModel.inputCheckAmount = "10.00"
+        calculatorViewModel.inputTipPercentage = ""
+
+        calculatorViewModel.calculateTip()
+
+        verify(mockCalculator, never()).calculateTip(ArgumentMatchers.anyDouble(), ArgumentMatchers.anyInt())
+
+    }
+
+
 
 
 }
