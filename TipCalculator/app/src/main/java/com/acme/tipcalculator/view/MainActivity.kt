963e773447766a8aa54d7e3767cae3b4ee0c4207
Russell Hicks <russell.hicks@bondhealth.co.uk>
2019-03-28 16:14:24 +0000

    Databinding to view model in place

--- TipCalculator/app/src/main/java/com/acme/tipcalculator/view/MainActivity.kt
+++ TipCalculator/app/src/main/java/com/acme/tipcalculator/view/MainActivity.kt
@@ -1,23 +1,31 @@
 package com.acme.tipcalculator.view
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import com.acme.tipcalculator.R
+import com.acme.tipcalculator.databinding.ActivityMainBinding
+import com.acme.tipcalculator.viewmodel.CalculatorViewModel
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.vm = CalculatorViewModel()
+
+
+        setSupportActionBar(binding.toolbar)
+
+
+
     }
 
 }
